be3d3b32a7c6886bb60de86970dac4ea7982b30b
Nathan Spencer <natekspencer@gmail.com>
2024-06-16 16:37:47 +0000

    Handle missing key in fields data

--- custom_components/pentair_cloud/helpers.py
+++ custom_components/pentair_cloud/helpers.py
@@ -3,12 +3,15 @@
 from __future__ import annotations
 
 from datetime import datetime
+import logging
 from time import time
 from typing import Any
 
 from pypentair.utils import get_api_field_name_and_value
 
 from homeassistant.util.dt import UTC
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def convert_timestamp(_ts: float) -> datetime:
@@ -18,5 +21,7 @@ def convert_timestamp(_ts: float) -> datetime:
 
 def get_field_value(key: str, data: dict) -> Any:
     """Get field value."""
-    name, value = get_api_field_name_and_value(key, data["fields"][key])
+    name, value = get_api_field_name_and_value(key, data["fields"].get(key))
+    if key not in data["fields"]:
+        _LOGGER.warning('%s key "%s" is missing in fields data', name, key)
     return value
